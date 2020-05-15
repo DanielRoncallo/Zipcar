863754a787e6d987488ac1cb534b7f04076766a3
DanielRoncallo <33792366+DanielRoncallo@users.noreply.github.com>
2020-05-15 11:51:18 -0500

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Config. Perfil</a></li>
+              <li><a href="update-password.php">Cambiar Contraseña</a></li>
+            <li><a href="my-booking.php">Reservas</a></li>
+            <li><a href="post-testimonial.php">Publicar un Testimonio</a></li>
+               <li><a href="my-testimonials.php">Mis Testimonios</a></li>
+            <li><a href="logout.php">Salir</a></li>
+          </ul>
+        </div>
+      </div>
